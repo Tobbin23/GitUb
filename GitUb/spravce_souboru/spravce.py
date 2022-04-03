9a5d8b299351fcfca496f0de39b1756989f41eda
Tob <67708830+Tobbin23@users.noreply.github.com>
2022-04-03 17:33:15 +0200

    Add files via upload

--- GitUb/spravce_souboru/spravce.py
+++ GitUb/spravce_souboru/spravce.py
@@ -1,1 +1,23 @@
+import os
+import sys
 
+def cesta(nazev:str, reposit:str):
+	
+	sjednoceni = os.path.join(os.path.dirname(os.path.realpath(__file__)),"Reposits",nazev)
+	
+	
+	if os.path.exists(os.path.join(sjednoceni)):
+		pp = os.chdir(sjednoceni)
+		print("ok")
+		# po kontrole souboru bude stahovat 
+		
+		down = os.system(" {path} & git clone {git}".format(path=pp,git=reposit))
+		sys.stdout.flush()
+	else:
+		# slozka nebyla nalezena, vytvorise a stazeni probehne 
+		os.mkdir(os.path.join(sjednoceni))
+		pp = os.chdir(sjednoceni)
+		print("byl vytroven..")  
+		down = os.system(" {path} & git clone {git}".format(path=pp,git=reposit))
+		sys.stdout.flush()
+				
