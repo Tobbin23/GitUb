7ac8610d953cb0cc758ea45cfc665ae01d575046
Tob <67708830+Tobbin23@users.noreply.github.com>
2022-04-16 15:32:23 +0200

    Add files via upload

--- GitUb/src/autor.py
+++ GitUb/src/autor.py
@@ -1,1 +1,28 @@
+#!/usr/bin/python3
+from .barvy import Barvy
+def logo_a():
+	__autor__= "Tobbin23"
+	__version__ = 2.2	
+	print( f"""
+	
+
+	{Barvy.OK} ██████╗ ██╗████████╗██╗   ██╗██████╗ 
+	██╔════╝ ██║╚══██╔══╝██║   ██║██╔══██╗
+	██║  ███╗██║   ██║   ██║   ██║██████╔╝
+	██║   ██║██║   ██║   ██║   ██║██╔══██╗
+	╚██████╔╝██║   ██║   ╚██████╔╝██████╔╝
+	╚═════╝ ╚═╝   ╚═╝    ╚═════╝ ╚═════╝{Barvy.RESET} 
+		{Barvy.BOLD}{Barvy.UNDLINE}Autor   | {__autor__}{Barvy.RESET}
+	        {Barvy.BOLD}{Barvy.UNDLINE}Version | {__version__}{Barvy.RESET}
+        __ 
+	""")
+def logo_b():
+	print(f"""
+	      {Barvy.BOLD}()(){Barvy.RESET}             ____ 
+	      {Barvy.BOLD}(..){Barvy.RESET}            {Barvy.WARNING}/|o  |{Barvy.RESET}
+	      {Barvy.BOLD}/\/\{Barvy.HEAD}  download{Barvy.RESET} {Barvy.WARNING}/o|  o|{Barvy.RESET}
+	     {Barvy.BOLD}c\db/o.........{Barvy.RESET}{Barvy.WARNING}/o_|_o_|{Barvy.RESET}
+
+	""")
+
 
