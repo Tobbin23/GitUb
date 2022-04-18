62575f29bdadeb18f1245cdfb6bd443b60f50a14
Tob <67708830+Tobbin23@users.noreply.github.com>
2022-04-18 16:09:20 +0200

    Add files via upload

---
+++ instalace.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+def priprava():
+	priprav = os.path.dirname(os.path.realpath(__file__))
+	spustitelny = "GitUb.desktop"
+	if os.path.exists(spustitelny):
+		instalace(spoustec=spustitelny)
+	else:
+		vytvor = subprocess.call(["touch", "GitUb.desktop"])
+		if os.path.exists(spustitelny):
+			instalace(spoustec=spustitelny)
+def instalace(spoustec):
+	path_1 = os.path.join(os.path.dirname(os.path.realpath(__file__)),"GitUb.py")
+	path_2 = os.path.join(os.path.dirname(os.path.realpath(__file__)),"icon/icon.ico")
+	
+	codo = "[Desktop Entry]\n"\
+		f"Icon={path_2}\n"\
+		f"Exec=python3 {path_1}\n"\
+		"Terminal=true\n"\
+		"Type=Application\n"\
+		"Categories=None\n"\
+		"StartupNotify=false\n"\
+		"Name[cs]=Inflační Kalkulačka"
+	with open(spoustec, mode="w") as zapis:
+		zapis.write(codo)
+#instalace()
+priprava()
